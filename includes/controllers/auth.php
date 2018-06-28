910bb60b6720b5adfff795fab9d3cb781a4b22c8
fernandomema <fernandomema@gmail.com>
2018-06-28 14:55:43 +0200

    Update auth.php
    fixed tabs (100%)

--- includes/controllers/auth.php
+++ includes/controllers/auth.php
@@ -1,32 +1,44 @@
 <?php
-
 include_once '../models/dbconn.php';
 
-class auth {
-    function register($username, $email, $password, $password2) {
-        //check if password mactch with repeat password input
+class auth
+
+{
+    function register($username, $email, $password, $password2)
+    {
+
+        // check if password mactch with repeat password input
+
         if ($password != $password2) {
             return 1;
-        } else {
+        }
+        else {
             $sql = "INSERT INTO users htmlentities($username), htmlentities($email), crypt($password)";
             $db = new db();
-					  // execute query
-            if(!$db->conn()->query($sql)) {
+
+            // execute query
+
+            if (!$db->conn()->query($sql)) {
                 echo 'could not create user';
-            }else {
+            }
+            else {
                 echo 'User registered succeful';
             }
         }
     }
-	
-	function login($username, $password) {
-	    $sql = "SELECT * FROM users WHERE email = htmlentities($username), password = crypt($password)";
-	  	$db = new db();
-		  // execute query
-      if(!$db->conn()->query($sql)fetch_array(MYSQLI_ASSOC);) {
-          echo 'User found';
-      }else {
-          echo 'User or password not found';
-      }	
-	 }
-} 
+
+    function login($username, $password)
+    {
+        $sql = "SELECT * FROM users WHERE email = htmlentities($username), password = crypt($password)";
+        $db = new db();
+
+        // execute query
+
+        if (!$db->conn()->query($sql)) {
+            echo 'User found';
+        }
+        else {
+            echo 'User or password not found';
+        }
+    }
+}

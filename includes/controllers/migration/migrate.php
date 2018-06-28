d691540207ccd74dbd96b7ee51110c02a17fb4e1
David Royo <droyovirgili@gmail.com>
2018-06-28 11:51:21 +0200

    Table users is now created automatically

--- includes/controllers/migration/migrate.php
+++ includes/controllers/migration/migrate.php
@@ -4,8 +4,34 @@ include_once '../../models/dbconn.php';
 
 class migrate extends db{
 	public function createTables() {
+		$sql = "
+
+		CREATE TABLE users (
+			id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+			firstname VARCHAR(30) NOT NULL,
+			lastname VARCHAR(30) NOT NULL,
+			email VARCHAR(50),
+			reg_date TIMESTAMP,
+			validated BOOLEAN NOT NULL DEFAULT 0
+		);
+
+		";
+
+		//CREATING CONNECTION AND EXECUTING QUERY
+		$db = new db();
 		
+		if(!$db->conn()->query($sql)) {
+			echo 'could not create table users';
+		}else {
+			echo 'Created table users';
+		}
+
 	}
 }
+
+
+//ONLY FOR TESTING PURPOSES
+$migrate = new migrate();
+$migrate->createTables();
 
 ?>

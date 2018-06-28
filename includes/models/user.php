cacf36e7b67fd06bad6cbda1f7485ce3112a9eac
David Royo <droyovirgili@gmail.com>
2018-06-28 15:17:17 +0200

    Added model user, with create method atm

--- includes/models/user.php
+++ includes/models/user.php
@@ -2,10 +2,41 @@
 
 include_once 'dbconn.php';
 
-class user {
-	protected function create() {
-		
-	}
+class user extends db{
+
+	private $firstname;
+	private $lastname;
+	private $email;
+	private $password;
+
+
+	public function create($firstname, $lastname, $email, $password) {
+
+		//DEFINE USER INFORMATION
+		$this->firstname = $firstname;
+		$this->lastname = $lastname;
+		$this->email = $email;
+		$this->password = $password;
+
+
+		//INSTANTIATING CLASS db AND DEFINING CONNECTION
+		$db = new db();
+		$conn = $db->conn();
+
+		//DEFINING OUR SQL QUERY
+		$sql = "
+		INSERT INTO users (firstname, lastname, email, password)
+		VALUES ('".$this->firstname."','".$this->lastname."','".$this->email."','".$this->password."');";
+
+		if(!$conn->query($sql)) {
+			//RETURN FALSE IF COULD NOT CREATE USER
+			echo $this->firstname . $this->lastname . $this->email . "<br>";
+			return false;
+		}else{
+			//RETURN TRUE IF USER IS CREATED
+			return true;
+		}
+	} 
 }
 
 ?>

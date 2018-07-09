4b1b291ce5381988fca59cace5fa28426cbba978
David Royo <droyovirgili@gmail.com>
2018-07-10 00:49:01 +0200

    Implementing the install functionality which will auto construct the database and create an admin account for the backoffice

---
+++ install/index.php
@@ -0,0 +1,8 @@
+<h1>Installation page</h1>
+
+<p>Here the user should be able to:</p>
+<ol>
+	<li>Enter the database credentials</li>
+	<li>Enter the credentials for the new admin account</li>
+	<li>That's it</li>
+</ol>

58aa1d308650fd67c56c6bb7b9657a5bb975e569
David Royo <droyovirgili@gmail.com>
2018-09-13 16:50:21 +0200

    Now it creates an addidional database, which will store the site's database credentials

--- includes/models/migrate.php
+++ includes/models/migrate.php
@@ -1,30 +1,54 @@
 <?php
 
-include_once './dbconn.php';
+include_once 'dbconn.php';
+
+if (session_status() == PHP_SESSION_NONE) {
+	session_start();
+}
+
 
 class migrate extends db{
 	public function createTables() {
-		$sql = "
+		$sql1 = "
 
 		CREATE TABLE users (
 			id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 			firstname VARCHAR(30) NOT NULL,
 			lastname VARCHAR(30) NOT NULL,
 			email VARCHAR(50) NOT NULL,
-			password nvarchar(128) NOT NULL,
+			password VARCHAR(128) NOT NULL,
 			reg_date TIMESTAMP,
 			validated BOOLEAN NOT NULL DEFAULT 0
 		);
+		";
 
+		$sql2 = "
+
+		CREATE TABLE dbinfo (
+			id INT(1) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+			servername VARCHAR(255) NOT NULL,
+			username VARCHAR(255) NOT NULL,
+			passwd VARCHAR(255) NOT NULL,
+			dbname VARCHAR(255) NOT NULL
+		);
 		";
 
 		//CREATING CONNECTION AND EXECUTING QUERY
 		$db = new db();
+
+		$sentence = $db->conn($_SESSION['servername'],$_SESSION['username'],$_SESSION['password'],$_SESSION['database']);
 		
-		if(!$db->conn()->query($sql)) {
-			echo 'Could not create table users, may already exist';
+		if (!$sentence->query($sql1)) {
+			echo('Error: '.$sentence->error.'<br>');
 		}else {
-			echo 'Created table users';
+			echo 'Created table users<br>';
+		}
+
+
+		if (!$sentence->query($sql2)) {
+			echo('Error: '.$sentence->error.'<br>');
+		}else {
+			echo 'Created table \'Database\'<br>';
 		}
 
 	}
@@ -36,3 +60,13 @@ class migrate extends db{
 //$migrate->createTables();
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Creating necessary stuff</title>
+</head>
+<body>
+
+</body>
+</html>
